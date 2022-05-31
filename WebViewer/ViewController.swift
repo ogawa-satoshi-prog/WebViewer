bce1ee46a72a1a61da09f4e03257a5b96675b870
ash <ogawa.sato01@gmail.com>
2022-06-01 02:08:02 +0900

    戻る・進むボタンの追加

--- WebViewer/ViewController.swift
+++ WebViewer/ViewController.swift
@@ -9,14 +9,12 @@ import UIKit
 import WebKit
 
 class ViewController: UIViewController,UITabBarDelegate {
-    @IBOutlet weak var webviewer: WKWebView!
+    @IBOutlet weak var webViewer: WKWebView!
     @IBOutlet weak var tabBar: UITabBar!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        let url = URL(string: "ランディングページのURL")
-//        let request = URLRequest(url: url!)
-//        webviewer.load(request)
+        
         self.tabBar.delegate = self
     }
     func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
@@ -24,18 +22,29 @@ class ViewController: UIViewController,UITabBarDelegate {
         case 0:
             let url = URL(string: "https://google.com")
             let request = URLRequest(url: url!)
-            webviewer.load(request)
+            webViewer.load(request)
         case 1:
             let url = URL(string: "https://yahoo.co.jp")
             let request = URLRequest(url: url!)
-            webviewer.load(request)
+            webViewer.load(request)
         case 2:
             let url = URL(string: "https://amazon.co.jp")
             let request = URLRequest(url: url!)
-            webviewer.load(request)
+            webViewer.load(request)
         default: return
         }
     }
     
+    @IBAction func backBtn(_ sender: Any) {
+        if(webViewer.canGoBack){
+            webViewer.goBack()
+        }
+    }
+    
+    @IBAction func forwardBtn(_ sender: Any) {
+        if(webViewer.canGoForward){
+            webViewer.goForward()
+        }
+    }
 }
 

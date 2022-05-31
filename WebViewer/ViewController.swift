514ef92ac33798c614099e78aeb9418bc3bf8484
ash <ogawa.sato01@gmail.com>
2022-06-01 01:54:27 +0900

    TabBarItemを選択して[google,yahoo,amazon]の３サイトを切り替えれるように

--- WebViewer/ViewController.swift
+++ WebViewer/ViewController.swift
@@ -6,14 +6,34 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITabBarDelegate {
+    @IBOutlet weak var webviewer: WKWebView!
+    @IBOutlet weak var tabBar: UITabBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.tabBar.delegate = self
     }
-
-
+    func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+        switch item.tag {
+        case 0:
+            let url = URL(string: "https://google.com")
+            let request = URLRequest(url: url!)
+            webviewer.load(request)
+        case 1:
+            let url = URL(string: "https://yahoo.co.jp")
+            let request = URLRequest(url: url!)
+            webviewer.load(request)
+        case 2:
+            let url = URL(string: "https://amazon.co.jp")
+            let request = URLRequest(url: url!)
+            webviewer.load(request)
+        default: return
+        }
+    }
+    
 }
 
